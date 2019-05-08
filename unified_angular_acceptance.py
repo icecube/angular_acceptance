9d634e961f84c573ecd8c0d956591393ac86d1f3
Philipp Eller <peller@phys.ethz.ch>
2019-05-08 16:30:38 -0400

    update

--- unified_angular_acceptance.py
+++ unified_angular_acceptance.py
@@ -1,9 +1,9 @@
-
 from __future__ import division
 import numpy as np
 from scipy.interpolate import make_interp_spline
 from scipy.integrate import quad
 
+# these values are obtained from a fit
 support_x = np.array([-1.  , -0.6 , -0.25,  0.1 ,  0.5 ,  0.9 ,  1.  ])
 components = np.array([[ 0.        ,  0.02185064, -0.02789483,  0.10264894,  0.15271317,
         -0.47946672, -0.85732023],
@@ -12,28 +12,26 @@ components = np.array([[ 0.        ,  0.02185064, -0.02789483,  0.10264894,  0.1
 mean = np.array([0.        , 0.10226867, 0.24635475, 0.36417989, 0.54721411,
        0.63978273, 0.58457966])
 
-def transform(p):
-    return np.dot(p, components) + mean
-
-
 def ang(params, values):
     """
-    New angular acceptance funcion
+    New angular acceptance function
     
     params : list / array
-        the two parametrrs p1 and p2
+        the two parametrrs p0 and p1
     values : float, list, array
         the eta values to compute the angular acceptance for in (-1, 1)
     """
+    # sanity check
     assert np.all(np.logical_and(np.greater_equal(values, -1), np.less_equal(values, 1))), 'values must be in range -1, 1'
-    transformed_params = transform(params)
-    f = make_interp_spline(support_x, transformed_params, bc_type=([(1, 0.0)], [(1, 0.0)]))
+    # inverse PCA transform
+    transformed_params = np.dot(params, components) + mean
+    # construct spline
+    f = make_interp_spline(support_x, transformed_params, bc_type=([(1, 0.)], [(1, 0.)]))
+    # make sure we're positive averywhere
     positive_f = lambda x : np.clip(f(x), 0., None)
-    
+    # normalize
     norm = quad(positive_f, -1, 1)
     out = positive_f(values)
     #normalize....why 0.68?
-    out /= norm[0] / 0.68
-    
+    out *= 0.68 / norm[0]
     return out
-
