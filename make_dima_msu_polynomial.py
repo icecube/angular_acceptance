53b20b7ef4a6c5718d034a2df0c0a2096c6073cc
Philipp Eller <peller@phys.ethz.ch>
2019-05-21 10:57:30 -0400

    Adding poly coeff function for dima+msu model

---
+++ make_dima_msu_polynomial.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+import numpy as np
+from scipy.integrate import quad
+from scipy.interpolate import make_interp_spline
+from numpy.polynomial import polynomial
+
+def msu(p, coseta):
+    '''
+    MSU parameterization 
+    https://wiki.icecube.wisc.edu/index.php/MSU_Forward_Hole_Ice
+    '''
+    f = lambda x : 0.34*(1. + 1.5 * x - x**3/2.) + p[0] * x * (x**2 - 1.)**3 + p[1] * np.exp(10.*(x - 1.2))
+    norm = quad(f, -1, 1)
+    return f(coseta) * 0.68 / norm[0]
+    return f(coseta)
+    
+def msu_poly(params):
+    """
+    Return standard IceCube format
+    
+    params : list/array
+        the parameters p0, p1, ... 
+    """
+    
+    x = np.linspace(-1,1,1001)
+    sampled = msu(params, x)
+    coeffs = np.zeros(12)
+    coeffs[0] = np.max(sampled)
+    coeffs[1:] = polynomial.polyfit(x, sampled, 10)
+    return coeffs
+    
+if __name__=='__main__':
+    from sys import argv
+    assert len(argv) == 3
+    params = [float(a) for a in argv[1:]]
+    print('\n'.join([str(a) for a in msu_poly(params)]))
